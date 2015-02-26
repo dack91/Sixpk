acecf48f1a75e2ca0599f9f5d4973412134bc8b0
dack91 <dack91@gmail.com>
2015-02-25 19:17:25 -0500

    update UI, add color, change from myruns template

--- app/src/main/java/edu/dartmouth/cs/codeit/sixpk/HistoryFragment.java
+++ app/src/main/java/edu/dartmouth/cs/codeit/sixpk/HistoryFragment.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -20,21 +21,27 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class HistoryFragment extends Fragment {
+    private Button showStats;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the view in the start xml
-        return inflater.inflate(R.layout.fragment_history, container, false);
+        View view = inflater.inflate(R.layout.fragment_history, container, false);
 
 
         // Show listview of past workouts
         // TODO
-    }
 
-
-    public void onShowStatsClicked(View v) {
-        // Go to statisticsActivity to see data visualization of workout history
-        Intent i = new Intent(getActivity(), StatisticsActivity.class);
-        startActivity(i);
+        // Start workout preview
+        showStats = (Button) view.findViewById(R.id.buttonShowStats);
+        showStats.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Go to statisticsActivity to see data visualization of workout history
+                Intent i = new Intent(getActivity(), StatisticsActivity.class);
+                startActivity(i);
+            }
+        });
+        return view;
     }
 
 
