433153dc463de1017bd5401f2f8092c4f6f74de5
dack91 <dack91@gmail.com>
2015-03-04 16:58:26 -0500

    updates to timer listener during workout

--- app/src/main/java/edu/dartmouth/cs/project/sixpk/WorkoutActivity.java
+++ app/src/main/java/edu/dartmouth/cs/project/sixpk/WorkoutActivity.java
@@ -3,12 +3,15 @@ package edu.dartmouth.cs.project.sixpk;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import edu.dartmouth.cs.project.sixpk.database.Workout;
 import edu.dartmouth.cs.project.sixpk.database.WorkoutEntryDataSource;
@@ -20,17 +23,25 @@ public class WorkoutActivity extends Activity {
     private boolean mPaused = false;
     private long mLastPause;
     private Workout mCurrWorkout;
+
     private WorkoutEntryDataSource dbHelper;
     private Context mContext;
 
     private long mWorkoutID;
-    private int[] mExerciseList;
-    private int[] mDurationList;
-    private int mCounter = 0;   // keep track of how many workouts were completed
-    private int TOTAL_TIME;
+    private int[] mExerciseList;    // list of all exercises in workouts
+    private int[] mDurationList;    // parallel list of all durations for exercises
 
-    private int mCurrExercise;
-    private int mTimeSoFar;
+    private int mCounter = 0;       // keep track of how many workouts were completed
+    private int TOTAL_TIME;         // full workout duration
+
+    private int mCurrExercise = 0;  // exercise ID for current exercise
+    private int mTimeSoFar = 0;     // increment after each exercise finishes
+
+    // Widgets for displaying current and next exercises
+    private ImageView mCurrExerciseImage;
+    private TextView mCurrExerciseText;
+    private ImageView mNextExerciseImage;
+    private TextView mNextExerciseText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +49,7 @@ public class WorkoutActivity extends Activity {
         setContentView(R.layout.activity_workout);
         mContext = this;
 
-
+        // Open database
         dbHelper = new WorkoutEntryDataSource(mContext);
         dbHelper.open();
 
@@ -50,6 +61,12 @@ public class WorkoutActivity extends Activity {
         // Get reference to pause stopwatch button
         mPause = (Button) findViewById(R.id.buttonPause);
 
+        // Get reference to current and next exercise textview and imageviews
+        mCurrExerciseImage = (ImageView) findViewById(R.id.imageViewCurrExercise);
+        mCurrExerciseText = (TextView) findViewById(R.id.workoutCurrExercise);
+        mNextExerciseImage = (ImageView) findViewById(R.id.imageViewNextExercise);
+        mNextExerciseText = (TextView) findViewById(R.id.workoutNextExercise);
+
         // Get current workout
         mWorkoutID = getIntent().getIntExtra(Globals.WORKOUT_ID_KEY, 0);
         mCurrWorkout = dbHelper.fetchWorkoutByIndex(mWorkoutID);
@@ -57,17 +74,15 @@ public class WorkoutActivity extends Activity {
         mDurationList = mCurrWorkout.getDurationList();
         TOTAL_TIME = mCurrWorkout.getDuration();
 
-        // TODO
-        // We might want this as a separate async task?
         // Set up timer listener to change exercise/finish workout
         mTimer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener()
         {
+            // Check to see if exercises should be updated
             @Override
             public void onChronometerTick(Chronometer chronometer)
             {
-                // Check to see if exercises should be updated
-                long currTime = SystemClock.elapsedRealtime();  // not sure this actually gets current time
-                Log.d("DEBUG", "Curr timer: " + currTime);
+                // Get current elapsed time
+                long currTime = (int) (SystemClock.elapsedRealtime() - mTimer.getBase()) / 1000;
 
                 // Last exercise completed
                 if (currTime == TOTAL_TIME) {
@@ -76,6 +91,7 @@ public class WorkoutActivity extends Activity {
 
                 // Update current and next exercise
                 else if (currTime == (mTimeSoFar + mDurationList[mCurrExercise])) {
+                    mTimeSoFar += mDurationList[mCurrExercise];
                     mCurrExercise++;
                     mCounter++;
                     nextExercise();
@@ -87,11 +103,32 @@ public class WorkoutActivity extends Activity {
 
     //TODO
     // Update current and next exercise
-    // update UI
-    // play beep sound
+    // play beep sound?
     public void nextExercise() {
         Log.d("DEBUG", "Updating to curr exercise: " + mExerciseList[mCurrExercise]);
+   //     mCurrExerciseImage.setImageURI(getExerciseImage(mCurrExercise));
+        mCurrExerciseText.setText(getExerciseText(mCurrExercise));
+
+        if (mCurrExercise < mExerciseList.length) {
+   //         mNextExerciseImage.setImageURI(getExerciseImage(mCurrExercise + 1));
+            mNextExerciseText.setText(getExerciseText(mCurrExercise + 1));
+        }
     }
+
+    public String getExerciseText(int index) {
+        String name = "";
+
+        // TODO
+        // get the exercise name at given index
+
+        return name;
+    }
+
+//    public Uri getExerciseImage(int index) {
+//        // TODO
+//        // get the exercise image at given index
+//        return new Uri();
+//    }
 
     // Saving all exercises completed so far, go to feedback
     public void onEndEarlyClicked(View v) {
